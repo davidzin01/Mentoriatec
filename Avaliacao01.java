1ac2360bb9a33813f6125d19745be553e45d3ca1
davidzin bb <david.xavier.2009@aluno.edu.pi.gov.br>
2025-09-09 12:50:33 -0300

    Add files via upload
    Primeira avaliação java

---
+++ Avaliacao01.java
@@ -0,0 +1,42 @@
+public class Avaliacao01 {
+
+    public static void main(String[] args) {
+      double n1 = 8.5;
+      double n2 = 7.3;
+      double n3 = 8.2;
+      double n4 = 5.0;
+      double n5 = 9.5;
+      double n6 = 6.0;
+      double n7 = 9.9;
+      double n8 = 4.7;
+
+      double mediaBimestre1 = (n1 + n2)/ 2;
+      double mediaBimestre2 = (n3 + n4) / 2;
+      double mediaBimestre3 = (n5 + n6) / 2;
+      double mediaBimestre4 = (n7 + n8) / 2;
+
+
+      double semestre1 = (mediaBimestre1 + mediaBimestre2) / 2;
+      double semestre2 = (mediaBimestre3 + mediaBimestre4) / 2;
+
+      System.out.println("Resultado Escolar");
+      System.out.printf(" 1º Bimestre: %.2f/n",mediaBimestre1);
+      System.out.printf(" 2º Bimestre: %.2f/n",mediaBimestre2);
+      System.out.printf("1ªsemestre: %.2f/n", semestre1);
+
+      System.out.printf(" 3º Bimestre: %.2f/n",mediaBimestre3);
+      System.out.printf(" 4º Bimestre: %.2f/n",mediaBimestre4);
+      System.out.printf(" 2º Semestre: %.2f/n",semestre2);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
